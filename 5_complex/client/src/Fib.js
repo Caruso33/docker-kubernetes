0c00687841da44e33ffd5b82dffd84c3f5d51abd
Tobias <tobias@appysphere.com>
2019-10-08 17:40:50 +0700

    build complex dev multi-container

---
+++ 5_complex/client/src/Fib.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+export default () => {
+  const [seenIndexes, setSeenIndexes] = useState([]);
+  const [values, setValues] = useState({});
+  const [index, setIndex] = useState("");
+
+  useEffect(() => {
+    fetchValues();
+    fetchIndexes();
+  }, []);
+
+  const fetchValues = async () => {
+    const values = await axios.get("/api/values/current");
+    setValues(values.data);
+  };
+
+  const fetchIndexes = async () => {
+    const indexes = await axios.get("/api/values/all");
+    setSeenIndexes(indexes.data);
+  };
+
+  const handleSubmit = async event => {
+    event.preventDefault();
+
+    await axios.post("/api/values", { index });
+    setIndex("");
+  };
+
+  const renderSeendIndexes = () => {
+    return seenIndexes.map(({ number }) => number).join(", ");
+  };
+
+  const renderSeenValues = () => {
+    const entries = [];
+
+    for (let key in values) {
+      entries.push(
+        <div key={key}>
+          For index {key} I calculated {values[key]}
+        </div>
+      );
+    }
+    return entries;
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>Enter your index:</label>
+        <input value={index} onChange={e => setIndex(e.target.value)} />
+        <button>Submit</button>
+      </form>
+
+      <h3>Indexes I have seen:</h3>
+      {renderSeendIndexes()}
+
+      <h3>Calculated Values:</h3>
+      {renderSeenValues()}
+    </div>
+  );
+};
